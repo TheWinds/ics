48010eefd217520bb8f925967018692c7582dff8
tracer-ics2017 <tracer@njuics.org>
2017-11-21 11:51:47 +0000

    >  compile 15033126 thewinds Linux f2eaeaf0aee4 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  11:51:47 up  5:19,  6 users,  load average: 0.01, 0.02, 0.00 a8d4ce7c3ba1e2dcdea6d651a3b891eb339db0a0

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -133,6 +133,7 @@ static int cmd_d(char* args){
 }
 
 static int cmd_x(char* args){
+  vaddr_write(1,4,-1);
   char *arg_n=strtok(NULL," ");
   if(arg_n==NULL){
     printf("usage: x [N] [ADDR EXPR]\n");
