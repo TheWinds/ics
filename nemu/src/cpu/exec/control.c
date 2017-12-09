ee5762622c71c7ba960714ee778075c9f59563d8
thewinds <wind_ns@163.com>
2017-12-09 13:44:19 +0000

    impl call

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,8 +25,9 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
-
+  // TODO();
+  decoding.jmp_eip=*eip+id_dest->val;
+  decoding.is_jmp=1;
   print_asm("call %x", decoding.jmp_eip);
 }
 
